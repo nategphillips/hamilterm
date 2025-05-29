9d77e06cfd7a1ceea8912085f2892c87c688a9c4
nategphillips <30729007+nategphillips@users.noreply.github.com>
2025-05-29 17:05:27 -0500

    feat: first version of hamilterm

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # hamilterm
+
 Symbolically computes the rotational Hamiltonian for a given molecular term symbol.
 
 ## License and Copyright
@@ -9,4 +10,4 @@ This program is free software: you can redistribute it and/or modify it under th
 
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
---
+++ src/main.py
@@ -0,0 +1,483 @@
+# module main
+"""Computes the first-order Hamiltonian for Σ and Π states."""
+
+# Copyright (C) 2025 Nathan G. Phillips
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from typing import cast
+
+import sympy as sp
+
+j_qn: sp.Symbol = sp.symbols("J")
+B, D, A, lamda, gamma, o, p, q, x = sp.symbols("B D A lambda gamma o p q x")
+
+include_r: bool = True
+include_so: bool = True
+include_ss: bool = True
+include_sr: bool = True
+include_ld: bool = True
+
+f_r, f_so, f_ss, f_sr, f_ld = map(int, [include_r, include_so, include_ss, include_sr, include_ld])
+
+LAMBDA_MAP: dict[str, int] = {"Sigma": 0, "Pi": 1}
+
+
+def mel_j2(j_qn: sp.Symbol) -> sp.Expr:
+    """Return the diagonal matrix element ⟨J|J^2|J⟩ = J(J + 1).
+
+    Args:
+        j_qn (sp.Symbol): Quantum number J
+
+    Returns:
+        sp.Expr: Matrix element J(J + 1)
+
+    """
+    return j_qn * (j_qn + 1)
+
+
+def mel_jz(omega_qn: sp.Rational) -> sp.Rational:
+    """Return the diagonal matrix element ⟨Ω|Jz|Ω⟩ = Ω.
+
+    Args:
+        omega_qn (sp.Rational): Quantum number Ω
+
+    Returns:
+        sp.Rational: Matrix element Ω
+
+    """
+    return omega_qn
+
+
+def mel_jp(j_qn: sp.Symbol, omega_qn_j: sp.Rational | sp.Expr) -> sp.Expr:
+    """Return the off-diagonal matrix element ⟨J, Ω - 1|J+|J, Ω⟩ = [J(J + 1) - Ω(Ω - 1)]^(1/2).
+
+    Args:
+        j_qn (sp.Symbol): Quantum number J
+        omega_qn_j (sp.Rational | sp.Expr): Quantum number Ω
+
+    Returns:
+        sp.Expr: Matrix element [J(J + 1) - Ω(Ω - 1)]^(1/2)
+
+    """
+    return sp.sqrt(j_qn * (j_qn + 1) - omega_qn_j * (omega_qn_j - 1))
+
+
+def mel_jm(j_qn: sp.Symbol, omega_qn_j: sp.Rational | sp.Expr) -> sp.Expr:
+    """Return the off-diagonal matrix element ⟨J, Ω + 1|J-|J, Ω⟩ = [J(J + 1) - Ω(Ω + 1)]^(1/2).
+
+    Args:
+        j_qn (sp.Symbol): Quantum number J
+        omega_qn_j (sp.Rational | sp.Expr): Quantum number Ω
+
+    Returns:
+        sp.Expr: Matrix element [J(J + 1) - Ω(Ω + 1)]^(1/2)
+
+    """
+    return sp.sqrt(j_qn * (j_qn + 1) - omega_qn_j * (omega_qn_j + 1))
+
+
+def mel_s2(s_qn: sp.Rational) -> sp.Rational:
+    """Return the diagonal matrix element ⟨S|S^2|S⟩ = S(S + 1).
+
+    Args:
+        s_qn (sp.Rational): Quantum number S
+
+    Returns:
+        sp.Rational: Matrix element S(S + 1)
+
+    """
+    return s_qn * (s_qn + 1)
+
+
+def mel_sz(sigma_qn: sp.Rational) -> sp.Rational:
+    """Return the diagonal matrix element ⟨Σ|Sz|Σ⟩ = Σ.
+
+    Args:
+        sigma_qn (sp.Rational): Quantum number Σ
+
+    Returns:
+        sp.Rational: Matrix element Σ
+
+    """
+    return sigma_qn
+
+
+def mel_sp(s_qn: sp.Rational, sigma_qn_j: sp.Rational | sp.Expr) -> sp.Expr:
+    """Return the off-diagonal matrix element ⟨S, Σ + 1|S+|S, Σ⟩ = [S(S + 1) - Σ(Σ + 1)]^(1/2).
+
+    Args:
+        s_qn (sp.Rational): Quantum number S
+        sigma_qn_j (sp.Rational | sp.Expr): Quantum number Σ
+
+    Returns:
+        sp.Expr: Matrix element [S(S + 1) - Σ(Σ + 1)]^(1/2)
+
+    """
+    return sp.sqrt(s_qn * (s_qn + 1) - sigma_qn_j * (sigma_qn_j + 1))
+
+
+def mel_sm(s_qn: sp.Rational, sigma_qn_j: sp.Rational | sp.Expr) -> sp.Expr:
+    """Return the off-diagonal matrix element ⟨S, Σ - 1|S-|S, Σ⟩ = [S(S + 1) - Σ(Σ - 1)]^(1/2).
+
+    Args:
+        s_qn (sp.Rational): Quantum number S
+        sigma_qn_j (sp.Rational | sp.Expr): Quantum number Σ
+
+    Returns:
+        sp.Expr: Matrix element [S(S + 1) - Σ(Σ - 1)]^(1/2)
+
+    """
+    return sp.sqrt(s_qn * (s_qn + 1) - sigma_qn_j * (sigma_qn_j - 1))
+
+
+def mel_lz(lambda_qn: int) -> int:
+    """Return the diagonal matrix element ⟨Λ|Lz|Λ⟩ = Λ.
+
+    Args:
+        lambda_qn (int): Quantum number Λ
+
+    Returns:
+        int: Matrix element Λ
+
+    """
+    return lambda_qn
+
+
+def h_rotational(
+    i: int, j: int, basis: list[tuple[int, sp.Rational, sp.Rational]], s_qn: sp.Rational
+) -> sp.Expr:
+    """Return matrix elements for the rotational Hamiltonian.
+
+    H_r = B * N^2 = B(J^2 + S^2 - 2JzSz - J+S- - J-S+).
+
+    Args:
+        i (int): Index i (row) of the Hamiltonian matrix
+        j (int): Index j (col) of the Hamiltonian matrix
+        basis (list[tuple[int, sp.Rational, sp.Rational]]): List of basis vectors |Λ, Σ; Ω>
+        s_qn (sp.Rational): Quantum number S
+
+    Returns:
+        sp.Expr: Matrix elements for B(J^2 + S^2 - 2JzSz - J+S- - J-S+)
+
+    """
+    _, sigma_qn_i, omega_qn_i = basis[i]
+    _, sigma_qn_j, omega_qn_j = basis[j]
+
+    # ⟨J, S, Ω, Σ|J^2 + S^2 - 2JzSz|J, S, Ω, Σ⟩ = J(J + 1) + S(S + 1) - 2ΩΣ
+    if i == j:
+        return B * (mel_j2(j_qn) + mel_s2(s_qn) - 2 * mel_jz(omega_qn_j) * mel_sz(sigma_qn_j))
+
+    # ⟨J, S, Ω - 1, Σ - 1|J+S-|J, S, Ω, Σ⟩ = ([J(J + 1) - Ω(Ω - 1)][S(S + 1) - Σ(Σ - 1)])^(1/2)
+    if omega_qn_i == omega_qn_j - 1 and sigma_qn_i == sigma_qn_j - 1:
+        return -B * (mel_jp(j_qn, omega_qn_j) * mel_sm(s_qn, sigma_qn_j))
+
+    # ⟨J, S, Ω + 1, Σ + 1|J-S+|J, S, Ω, Σ⟩ = ([J(J + 1) - Ω(Ω + 1)][S(S + 1) - Σ(Σ + 1)])^(1/2)
+    if omega_qn_i == omega_qn_j + 1 and sigma_qn_i == sigma_qn_j + 1:
+        return -B * (mel_jm(j_qn, omega_qn_j) * mel_sp(s_qn, sigma_qn_j))
+
+    return sp.S.Zero
+
+
+def h_spin_orbit(i: int, j: int, basis: list[tuple[int, sp.Rational, sp.Rational]]) -> sp.Expr:
+    """Return matrix elements for the spin-orbit Hamiltonian.
+
+    H_so = A(Lz * Sz).
+
+    Args:
+        i (int): Index i (row) of the Hamiltonian matrix
+        j (int): Index j (col) of the Hamiltonian matrix
+        basis (list[tuple[int, sp.Rational, sp.Rational]]): List of basis vectors |Λ, Σ; Ω>
+
+    Returns:
+        sp.Expr: Matrix elements for A(Lz * Sz)
+
+    """
+    lambda_qn_j, sigma_qn_j, _ = basis[j]
+
+    # ⟨Λ, Σ|LzSz|Λ, Σ⟩ = ΛΣ
+    if i == j:
+        return A * mel_lz(lambda_qn_j) * mel_sz(sigma_qn_j)
+
+    return sp.S.Zero
+
+
+def h_spin_spin(
+    i: int, j: int, basis: list[tuple[int, sp.Rational, sp.Rational]], s_qn: sp.Rational
+) -> sp.Expr:
+    """Return matrix elements for the spin-spin Hamiltonian.
+
+    H_ss = 2/3 * λ(3Sz^2 - S^2).
+
+    Args:
+        i (int): Index i (row) of the Hamiltonian matrix
+        j (int): Index j (col) of the Hamiltonian matrix
+        basis (list[tuple[int, sp.Rational, sp.Rational]]): List of basis vectors |Λ, Σ; Ω>
+        s_qn (sp.Rational): Quantum number S
+
+    Returns:
+        sp.Expr: Matrix elements for 2/3 * λ(3Sz^2 - S^2)
+
+    """
+    sigma_qn_j: sp.Rational = basis[j][1]
+
+    # ⟨S, Σ|3Sz^2 - S^2|S, Σ⟩ = 3Σ^2 − S(S + 1)
+    if i == j:
+        return sp.Rational(2, 3) * lamda * (3 * mel_sz(sigma_qn_j) ** 2 - mel_s2(s_qn))
+
+    return sp.S.Zero
+
+
+def h_spin_rotation(
+    i: int, j: int, basis: list[tuple[int, sp.Rational, sp.Rational]], s_qn: sp.Rational
+) -> sp.Expr:
+    """Return matrix elements for the spin-rotation Hamiltonian.
+
+    H_sr = γ(N·S) = γ[JzSz + 0.5 * (J+S- + J-S+) - S^2].
+
+    Args:
+        i (int): Index i (row) of the Hamiltonian matrix
+        j (int): Index j (col) of the Hamiltonian matrix
+        basis (list[tuple[int, sp.Rational, sp.Rational]]): List of basis vectors |Λ, Σ; Ω>
+        s_qn (sp.Rational): Quantum number S
+
+    Returns:
+        sp.Expr: Matrix elements for γ[JzSz + 0.5 * (J+S- + J-S+) - S^2]
+
+    """
+    _, sigma_qn_i, omega_qn_i = basis[i]
+    _, sigma_qn_j, omega_qn_j = basis[j]
+
+    # ⟨S, Ω, Σ|JzSz - S^2|S, Ω, Σ⟩ = ΩΣ - S(S + 1)
+    if i == j:
+        return gamma * (mel_jz(omega_qn_j) * mel_sz(sigma_qn_j) - mel_s2(s_qn))
+
+    # ⟨J, S, Ω - 1, Σ - 1|J+S-|J, S, Ω, Σ⟩ = ([J(J + 1) - Ω(Ω - 1)][S(S + 1) - Σ(Σ - 1)])^(1/2)
+    if sigma_qn_i == sigma_qn_j - 1 and omega_qn_i == omega_qn_j - 1:
+        return gamma * (mel_jp(j_qn, omega_qn_j) * mel_sm(s_qn, sigma_qn_j)) / 2
+
+    # ⟨J, S, Ω + 1, Σ + 1|J-S+|J, S, Ω, Σ⟩ = ([J(J + 1) - Ω(Ω + 1)][S(S + 1) - Σ(Σ + 1)])^(1/2)
+    if sigma_qn_i == sigma_qn_j + 1 and omega_qn_i == omega_qn_j + 1:
+        return gamma * (mel_jm(j_qn, omega_qn_j) * mel_sp(s_qn, sigma_qn_j)) / 2
+
+    return sp.S.Zero
+
+
+def h_lambda_doubling(
+    i: int, j: int, basis: list[tuple[int, sp.Rational, sp.Rational]], s_qn: sp.Rational
+) -> sp.Expr:
+    """Return matrix elements for the lambda doubling Hamiltonian.
+
+    H_ld = 0.5(o + p + q)(S+^2 + S-^2) - 0.5(p + 2q)(J+S+ + J-S-) + 0.5 * q(J+^2 + J-^2).
+
+    Args:
+        i (int): Index i (row) of the Hamiltonian matrix
+        j (int): Index j (col) of the Hamiltonian matrix
+        basis (list[tuple[int, sp.Rational, sp.Rational]]): List of basis vectors |Λ, Σ; Ω>
+        s_qn (sp.Rational): Quantum number S
+
+    Returns:
+        sp.Expr: Matrix elements for 0.5(o + p + q)(S+^2 + S-^2) - 0.5(p + 2q)(J+S+ + J-S-) +
+            0.5 * q(J+^2 + J-^2)
+
+    """
+    lambda_qn_i, sigma_qn_i, omega_qn_i = basis[i]
+    lambda_qn_j, sigma_qn_j, omega_qn_j = basis[j]
+
+    # Only allow Λ ± 2 transitions, return early if not.
+    if abs(lambda_qn_i - lambda_qn_j) != 2:
+        return sp.S.Zero
+
+    # ⟨Σ + 2|S+^2|Σ⟩ = 0.5(o + p + q)([S(S + 1) - Σ(Σ + 1)][S(S + 1) - (Σ + 1)(Σ + 2)])^(1/2)
+    if lambda_qn_i == lambda_qn_j - 2 and sigma_qn_i == sigma_qn_j + 2:
+        return 0.5 * (o + p + q) * mel_sp(s_qn, sigma_qn_j) * mel_sp(s_qn, sigma_qn_j + 1)
+
+    # ⟨Σ - 2|S-^2|Σ⟩ = 0.5(o + p + q)([S(S + 1) - Σ(Σ - 1)][S(S + 1) - (Σ - 1)(Σ - 2)])^(1/2)
+    if lambda_qn_i == lambda_qn_j + 2 and sigma_qn_i == sigma_qn_j - 2:
+        return 0.5 * (o + p + q) * mel_sm(s_qn, sigma_qn_j) * mel_sm(s_qn, sigma_qn_j - 1)
+
+    # ⟨Ω - 1, Σ + 1|J+S+|Ω, Σ⟩ = -0.5(p + 2q)([J(J + 1) - Ω(Ω - 1)][S(S + 1) - Σ(Σ + 1)])^(1/2)
+    if (
+        lambda_qn_i == lambda_qn_j - 2
+        and sigma_qn_i == sigma_qn_j + 1
+        and omega_qn_i == omega_qn_j - 1
+    ):
+        return -0.5 * (p + 2 * q) * mel_jp(j_qn, omega_qn_j) * mel_sp(s_qn, sigma_qn_j)
+
+    # ⟨Ω + 1, Σ - 1|J-S-|Ω, Σ⟩ = -0.5(p + 2q)([J(J + 1) - Ω(Ω + 1)][S(S + 1) - Σ(Σ - 1)])^(1/2)
+    if (
+        lambda_qn_i == lambda_qn_j + 2
+        and sigma_qn_i == sigma_qn_j - 1
+        and omega_qn_i == omega_qn_j + 1
+    ):
+        return -0.5 * (p + 2 * q) * mel_jm(j_qn, omega_qn_j) * mel_sm(s_qn, sigma_qn_j)
+
+    # NOTE: 25/05/29 - The Ω - 1 being plugged into the second J+ matrix element occurs since J is
+    #       an anomalously commutative operator.
+    # ⟨Ω - 2|J+^2|Ω⟩ = 0.5 * q([J(J + 1) - Ω(Ω - 1)][J(J + 1) - (Ω - 1)(Ω - 2)])^(1/2)
+    if lambda_qn_i == lambda_qn_j - 2 and omega_qn_i == omega_qn_j - 2:
+        return 0.5 * q * mel_jp(j_qn, omega_qn_j) * mel_jp(j_qn, omega_qn_j - 1)
+
+    # NOTE: 25/05/29 - The same thing happens here with Ω + 1.
+    # ⟨Ω + 2|J-^2|Ω⟩ = 0.5 * q([J(J + 1) - Ω(Ω + 1)][J(J + 1) - (Ω + 1)(Ω + 2)])^(1/2)
+    if lambda_qn_i == lambda_qn_j + 2 and omega_qn_i == omega_qn_j + 2:
+        return 0.5 * q * mel_jm(j_qn, omega_qn_j) * mel_jm(j_qn, omega_qn_j + 1)
+
+    return sp.S.Zero
+
+
+def build_hamiltonian(
+    basis: list[tuple[int, sp.Rational, sp.Rational]], s_qn: sp.Rational
+) -> sp.MutableDenseMatrix:
+    """Build the symbolic Hamiltonian matrix.
+
+    Args:
+        basis (list[tuple[int, sp.Rational, sp.Rational]]): List of basis vectors |Λ, Σ; Ω>
+        s_qn (sp.Rational): Quantum number S
+
+    Returns:
+        sp.MutableDenseMatrix: Hamiltonian matrix H
+
+    """
+    h_mat: sp.MutableDenseMatrix = sp.zeros(len(basis))
+
+    # TODO: 25/05/29 - Use the molecular term symbol to see which terms need to be considered and
+    #       only compute those terms.
+    for i in range(len(basis)):
+        for j in range(len(basis)):
+            h_mat[i, j] = (
+                f_r * h_rotational(i, j, basis, s_qn)
+                + f_so * h_spin_orbit(i, j, basis)
+                + f_ss * h_spin_spin(i, j, basis, s_qn)
+                + f_sr * h_spin_rotation(i, j, basis, s_qn)
+                + f_ld * h_lambda_doubling(i, j, basis, s_qn)
+            )
+
+    return h_mat
+
+
+def parse_term_symbol(term_symbol: str) -> tuple[sp.Rational, int]:
+    """Parse the molecular term symbol into the quantum numbers S and Λ.
+
+    Args:
+        term_symbol (str): Molecular term symbol, e.g., "2Pi" or "3Sigma"
+
+    Returns:
+        tuple[sp.Rational, int]: Quantum numbers S and Λ
+
+    """
+    spin_multiplicity: int = int(term_symbol[0])
+    s_qn: sp.Rational = safe_rational(spin_multiplicity - 1, 2)
+    term: str = term_symbol[1:]
+    lambda_qn: int = LAMBDA_MAP[term]
+
+    return s_qn, lambda_qn
+
+
+def generate_basis(s_qn: sp.Rational, lambda_qn: int) -> list[tuple[int, sp.Rational, sp.Rational]]:
+    """Construct the Hund's case (a) basis set |Λ, Σ; Ω>.
+
+    Args:
+        s_qn (sp.Rational): Quantum number S
+        lambda_qn (int): Quantum number Λ
+
+    Returns:
+        list[tuple[int, sp.Rational, sp.Rational]]: List of basis vectors |Λ, Σ; Ω>
+
+    """
+    # Possible values for Σ = S, S - 1, ..., -S. There are 2S + 1 total values of Σ.
+    sigmas: list[sp.Rational] = [
+        cast("sp.Rational", -s_qn + safe_rational(i, 1)) for i in range(int(2 * s_qn) + 1)
+    ]
+
+    # For states with Λ > 1, include both +Λ and -Λ in the basis.
+    lambdas: list[int] = [lambda_qn] if lambda_qn == 0 else [-lambda_qn, lambda_qn]
+    basis: list[tuple[int, sp.Rational, sp.Rational]] = []
+
+    for lam in lambdas:
+        for sigma in sigmas:
+            omega: sp.Rational = cast("sp.Rational", lam + sigma)
+            basis.append((lam, sigma, omega))
+
+    return basis
+
+
+def safe_rational(num: int, denom: int) -> sp.Rational:
+    """Ensure a rational return value.
+
+    Args:
+        num (int): Numerator
+        denom (int): Denominator
+
+    Raises:
+        ValueError: If NaN or Infinity is encountered
+
+    Returns:
+        sp.Rational: A guaranteed sp.Rational type
+
+    """
+    r = sp.Rational(num, denom)
+
+    if not isinstance(r, sp.Rational):
+        raise ValueError(f"Expected sp.Rational, got {r}.")
+
+    return cast("sp.Rational", r)
+
+
+def fsn(num: int | sp.Rational | sp.Expr) -> str:
+    """Format signed number for printing.
+
+    Args:
+        num (int | sp.Rational | sp.Expr): Number
+
+    Returns:
+        str: Print-friendly string with a +, ±, or -
+
+    """
+    s: str = str(num)
+
+    if num > 0:
+        return f"+{s}"
+    if num == 0:
+        return f"±{s}"
+
+    return s
+
+
+def main() -> None:
+    """Entry point."""
+    term_symbol: str = "2Pi"
+
+    s_qn, lambda_qn = parse_term_symbol(term_symbol)
+    basis: list[tuple[int, sp.Rational, sp.Rational]] = generate_basis(s_qn, lambda_qn)
+    h_mat: sp.MutableDenseMatrix = build_hamiltonian(basis, s_qn)
+    eigenvals: dict[sp.Expr, int] = cast("dict[sp.Expr, int]", h_mat.eigenvals())
+
+    print("Term symbol:")
+    print(f"{term_symbol}: S={s_qn}, Λ={lambda_qn}")
+
+    print("\nBasis states |Λ, Σ, Ω>:")
+    for state in basis:
+        print(f"|{fsn(state[0])}, {fsn(state[1])}, {fsn(state[2])}>")
+
+    print("\nHamiltonian matrix:")
+    sp.pprint(h_mat.subs(j_qn * (j_qn + 1), x).applyfunc(sp.simplify))
+
+    print("\nEigenvalues:")
+    for eigenvalue in eigenvals:
+        sp.pprint(sp.nsimplify(eigenvalue))
+
+
+if __name__ == "__main__":
+    main()
